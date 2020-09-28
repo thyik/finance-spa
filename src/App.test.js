844169fbbfcf2258bc57c073e315272f62f494e4
yikth <netyth@gmail.com>
2020-09-28 16:29:43 +0800

    fix test case

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders finance spa text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/finance SPA/i);
   expect(linkElement).toBeInTheDocument();
 });
